e57672f62982c5656a5bf42186fa61ade8df7dbe
Atraski <atraskiwebsite@gmail.com>
2023-11-25 15:50:20 +0530

    first commit

---
+++ Frontend/src/components/Atstaynextprice.js
@@ -0,0 +1,474 @@
+import page1img1 from '../images/page1img1.webp'
+import page1img2 from '../images/page1img2.webp'
+import page2img1 from '../images/page2img1.webp'
+import page2img2 from '../images/page2img2.webp'
+import page2img3 from '../images/page2img3.webp'
+import page2img4 from '../images/page2img4.webp'
+import page2img5 from '../images/page2img5.webp'
+import page3img1 from '../images/page3img1.webp'
+import page3img2 from '../images/page3img2.webp'
+import page3img3 from '../images/page3img3.webp'
+import page3img4 from '../images/page3img4.webp'
+import page3img5 from '../images/page3img5.webp'
+import page3img6 from '../images/page3img6.webp'
+import page4img1 from '../images/page4img1.webp'
+import page5img1 from '../images/page5img1.webp'
+import page5img2 from '../images/page5img2.webp'
+import page5img3 from '../images/page5img3.webp'
+import page5img4 from '../images/page5img4.webp'
+import page5img5 from '../images/page5img5.webp'
+import pag6img1 from '../images/pag6img1.webp'
+import pag6img2 from '../images/pag6img2.webp'
+import pag6img3 from '../images/pag6img3.webp'
+import page7img1 from '../images/page7img1.webp'
+import page7img2 from '../images/page7img2.webp'
+import page8img1 from '../images/page8img1.webp'
+import page8img2 from '../images/page8img2.webp'
+import page8img3 from '../images/page8img3.webp'
+import page8img4 from '../images/page8img4.webp'
+import page9img1 from '../images/page9img1.webp'
+import page9img2 from '../images/page9img2.webp'
+import page9img3 from '../images/page9img3.webp'
+import page9img4 from '../images/page9img4.webp'
+import page9img5 from '../images/page9img5.webp'
+import page9img6 from '../images/page9img6.webp'
+import page10img1 from '../images/page10img1.webp'
+
+
+export const productData6=[
+    {
+        id:1,
+        price:8000.00,
+        imgs:page1img1,roomtype:'Deluxe Double Room',
+        place : "Beledanga, West Bengal",
+        
+    },
+
+    {
+        id:2,
+        price:8000,
+        imgs:page1img2,roomtype:'Deluxe Single Room',
+        place : "Beledanga, West Bengal",
+    },
+
+    {
+        id:3,
+        price:13000,
+        imgs:page2img1,roomtype:'Rangbhang Manor with valley view',
+        place : "Darjeeling, West Bengal",
+    },
+
+    {
+        id:4,
+        price:13000,
+        imgs:page2img2,roomtype:'Tealands Suite with infinite view',
+        place : "Darjeeling, West Bengal",
+    },
+
+    {
+        id:5,
+        price:13000,
+        imgs:page2img3,roomtype:'Duke & Duchess with panoramic view',
+        place : "Darjeeling, West Bengal",
+    },
+
+    {
+        id:6,
+        price:13000,
+        imgs:page2img4,roomtype:'Golden Manor',
+        place : "Darjeeling, West Bengal",
+    },
+
+    {
+        id:7,
+        price:13000,
+        imgs:page2img5,roomtype:'Mile-end Manor',
+        place : "Darjeeling, West Bengal",
+    },
+
+    {
+        id:8,
+        price:8000,
+        imgs:page3img1,roomtype:'Notun-Dak Bungalow',
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:9,
+        price:8000,
+        imgs:page3img2,roomtype:'  Dak Bungalow',
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:10,
+        price:8000,
+        imgs:page3img3,roomtype:' Royal Suite',
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:11,
+        price:8000,
+        imgs:page3img4,roomtype:'Zamindari Suite ' ,
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:12,
+        price:8000,
+        imgs:page3img5,roomtype:'Classic Heritage',
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:13,
+        price:8000,
+        imgs:page3img6,roomtype:'Notun Bari' ,
+        place : "Kolkata, West Bengal",
+    },
+
+    {
+        id:14,
+        price:5500,
+        imgs:page4img1,roomtype:'Heritage Stay',
+        place : "Kerala",
+    },
+
+    {
+        id:15,
+        price:12665,
+        imgs:page5img1,roomtype:' 2 Bed room Cottage',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:16,
+        price:12665,
+        imgs:page5img2,roomtype:'Family Room    ',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:17,
+        price:12665,
+        imgs:page5img3,roomtype:'Premier  ',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:18,
+        price:12665,
+        imgs:page5img4,roomtype:'Grand',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:19,
+        price:12665,
+        imgs:page5img5,roomtype:'classic',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:20,
+        price:8000,
+        imgs:pag6img1,roomtype:'Luxury suite    ',
+        place : "Jammu & Kashmir",
+    },
+
+    {
+        id:21,
+        price:8000,
+        imgs:pag6img2,roomtype:'Heritage suite',
+        place : "Jammu & Kashmir",
+    },
+
+    {
+        id:22,
+        price:8000,
+        imgs:pag6img3,roomtype:'Platinum delight',
+        place : "Jammu & Kashmir",
+    },
+
+    {
+        id:23,
+        price:17400,
+        imgs:page7img1,roomtype:'Royal Suite Room',
+        place : "Vadodara, Gujarat",
+    },
+
+    {
+        id:24,
+        price:17400,
+        imgs:page7img2,roomtype:'Heritage suite Room ',
+        place : "Vadodara, Gujarat",
+    },
+
+    {
+        id:25,
+        price:7000,
+        imgs:page8img1,roomtype:'Turquoise',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:26,
+        price:7000,
+        imgs:page8img2,roomtype:'Saffire',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:27,
+        price:7000,
+        imgs:page8img3,roomtype:'Ruby',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:28,
+        price:7000,
+        imgs:page8img4,roomtype:'Coral',
+        place : "Himachal Pradesh",
+    },
+
+    {
+        id:29,
+        price:2900,
+        imgs:page9img1,roomtype:'Maharaja Suite',
+        place : "Rajasthan"
+    },
+
+    {
+        id:30,
+        price:2900,
+        imgs:page9img2,roomtype:'Apple Suite ',
+        place : "Rajasthan"
+    },
+
+    {
+        id:31,
+        price:2900,
+        imgs:page9img3,roomtype:'Boho Suite',
+        place : "Rajasthan"
+    },
+
+    {
+        id:32,
+        price:2900,
+        imgs:page9img4,roomtype:'Walnut Suite',
+        place : "Rajasthan"
+    },
+
+    {
+        id:33,
+        price:2900,
+        imgs:page9img5,roomtype:'Oak Suite',
+        place : "Rajasthan"
+    },
+
+    {
+        id:34,
+        price:2900,
+        imgs:page9img6,roomtype:'Luxury Tents',
+        place : "Rajasthan"
+    },
+
+    {
+        id:35,
+        price:4200,
+        imgs:page10img1,roomtype:'Traditional Huts',
+        place : "Madhya Pradesh",
+    },
+
+    {
+        id:36,
+        price:2217,
+        imgs:page10img1,roomtype:'ATSTAY Bhimkothi Boutique 1011',
+        place : "Jaisalimer,Rajasthan",
+    },
+
+    {
+        id:37,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Shangri-La The Heritage Haveli 1012',
+        place : "Jodhpur, Rajasthan",
+    },
+
+    {
+        id:38,
+        price:2094,
+        imgs:page10img1,roomtype:'ATSTAY Mowgli Town Homestay 1013',
+        place : "Jeolikote, Uttarakhand",
+    },
+
+    {
+        id:39,
+        price:3080,
+        imgs:page10img1,roomtype:'ATSTAY Stonewood Homestay 1014',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+
+    {
+        id:40,
+        price:3696,
+        imgs:page10img1,roomtype:'ATSTAY Shimla Hills Homestay Kamal 1015',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+    {
+        id:41,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY The City Escape Farmstay 1016',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+    {
+        id:42,
+        price:3942,
+        imgs:page10img1,roomtype:'ATSTAY Royce Villa Nainital 1017',
+        place : "Nainital, Uttarakhand",
+    },
+
+    {
+        id:43,
+        price:8288,
+        imgs:page10img1,roomtype:'ATSTAY The Hive Pushkar 1018',
+        place : "Pushkar, Rajasthan",
+    },
+
+    {
+        id:44,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY The Hive Jaipur 1019',
+        place : "Jaipur, Rajasthanh",
+    },
+
+    {
+        id:45,
+        price:985,
+        imgs:page10img1,roomtype:'ATSTAY Kappu Home Stay 1020',
+        place : "Sainj, Himachal Pradesh",
+    },
+
+    {
+        id:46,
+        price:3080,
+        imgs:page10img1,roomtype:'ATSTAY Himri Mountains Home Stay 1021',
+        place : "Jangal Drabala, Himachal Pradesh",
+    },
+
+
+    {
+        id:47,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Sunbreeze shimla 1022',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+
+    {
+        id:48,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Sunbeam 3bhk apartment 1023',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+    {
+        id:49,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Cedarwood villa 1024',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+    {
+        id:50,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Jakhoo nest 1025',
+        place : "Shimla, Himachal Pradesh",
+    },
+
+    {
+        id:51,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Escape Town Homestay 1026',
+        place : "Manali, Himachal Pradesh",
+    },
+
+    {
+        id:52,
+        price:1355,
+        imgs:page10img1,roomtype:'ATSTAY KANAK HOMESTAY 1027',
+        place : "Uttar Pradesh",
+    },
+
+    {
+        id:53,
+        price:2800,
+        imgs:page10img1,roomtype:'ATSTAY Pushpanjali Homestay 1028',
+        place : "Varanasi, Uttar Pradesh",
+    },
+
+    {
+        id:54,
+        price:1478,
+        imgs:page10img1,roomtype:'ATSTAY Ramda Guest House 1029',
+        place : "Maichor, Uttarakhand",
+    },
+
+    {
+        id:55,
+        price:4534,
+        imgs:page10img1,roomtype:'ATSTAY Kasar Himalaya holiday home 1030',
+        place : "Binsar road, Uttarakhand",
+    },
+
+    {
+        id:56,
+        price:2217,
+        imgs:page10img1,roomtype:'ATSTAY Binsar view home stay 1031',
+        place : "Uttarakhand",
+    },
+
+    {
+        id:57,
+        price:3000,
+        imgs:page10img1,roomtype:'ATSTAY Oxygen HomeStay 1032',
+        place : "Uttarakhand",
+    },
+
+    {
+        id:58,
+        price:5600,
+        imgs:page10img1,roomtype:'ATSTAY The Bougainvillea - Luxury Homestay in Dehradun 1033',
+        place : "Dehradun, Uttarakhand",
+    },
+
+    {
+        id:59,
+        price:3080,
+        imgs:page10img1,roomtype:'ATSTAY BBC Homestays Dehradun 1034',
+        place : "Sudhowala, Uttarakhand",
+    },
+
+    {
+        id:60,
+        price:4928,
+        imgs:page10img1,roomtype:'ATSTAY Jhinuk Homestay 1035',
+        place : "Panaji, Goa",
+    },
+
+    {
+        id:61,
+        price:1540,
+        imgs:page10img1,roomtype:'ATSTAY MMM Home Rentals 1036',
+        place : "Dehradun, Uttarakhand",
+    }
+
+   
+   
+
+    
+]
